66b92c3e1106bd5ff1dba0c259aba3274106c418
Tomas Briones <tbriones@economial.cl>
2021-04-12 21:57:00 -0400

    add argument error

--- desafio1_cuentabancaria/cartas.rb
+++ desafio1_cuentabancaria/cartas.rb
@@ -1,15 +1,20 @@
 class Carta
-
+    attr_reader :numero, :pinta
     #inicializo el metodo constructor con las variables que tengo que definir.
     def initialize(numero, pinta)
 
         pinta.upcase!
+        pinta.to_s!
         pinta = ['C','D','E','T']
 
         #evaluamos si esta entre el rango correcto, evaluamos que la pinta sea la correcta
         if (numero >= 1 && numero <= 13) && pinta.include?(pinta)
             @numero = numero
             @pinta = pinta
+        else
+            raise ArgumentError.new('No es un argumento válido') #levanta un error
         end
     end
+    #'f', pinta que no existe, entra igual al initialize
+    #require_relative 'desafio1_cuentabancaria/cartas'
 end
