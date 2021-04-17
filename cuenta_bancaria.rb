f8e8985e43ed89da66a5ada592e1b868644f1014
Tomas Briones <tbriones@economial.cl>
2021-04-17 00:45:11 -0400

    code with rRange and Argument Error

--- cuenta_bancaria.rb
+++ cuenta_bancaria.rb
@@ -2,12 +2,21 @@ class CuentaBancaria
 
     attr_accessor :nombre_de_usuario
 
-    def initialize(nombre_usuario,numero_cuenta)
-        @nombre_de_usuario = nombre_usuario
-        @numero_de_cuenta = numero_cuenta
+    def initialize(nombre_usuario,numero_cuenta,vip=0)
+        if numero_cuenta.is_a?(Integer)
+            if (numero_cuenta.digits).count == 8
+                @nombre_de_usuario = nombre_usuario.to_s
+                @numero_de_cuenta = numero_cuenta
+                @vip = vip
+            else
+                raise RangeError.new("Numero de cuenta fuera de Rango")
+            end
+        else
+            raise ArgumentError.new("'#{numero_cuenta}' no es un argumento válido")
+        end
     end
 end
 
-cuenta1 = CuentaBancaria.new("tomas","cualquier cosa")
+cuenta1 = CuentaBancaria.new("tomas",) #si la cuenta comienza con ceros, va a contar los digitos, descontando los ceros
 
 puts "#{cuenta1.nombre_de_usuario}"
