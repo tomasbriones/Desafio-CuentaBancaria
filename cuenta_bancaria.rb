a59d30060c9bf3e12caef7f8d07f257f7de43281
Tomas Briones <tbriones@economial.cl>
2021-04-17 00:25:47 -0400

    testing initialize

--- cuenta_bancaria.rb
+++ cuenta_bancaria.rb
@@ -1,5 +1,13 @@
 class CuentaBancaria
-    def initialize
-        
+
+    attr_accessor :nombre_de_usuario
+
+    def initialize(nombre_usuario,numero_cuenta)
+        @nombre_de_usuario = nombre_usuario
+        @numero_de_cuenta = numero_cuenta
     end
 end
+
+cuenta1 = CuentaBancaria.new("tomas","cualquier cosa")
+
+puts "#{cuenta1.nombre_de_usuario}"
